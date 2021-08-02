81f7e2b6580065046a6cef97205b5fb0ede830a6
AndrewEhrhardt <andyehrhardt@gmail.com>
2021-08-02 13:35:32 -0500

    completed questions, still needs commenting

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -36,10 +36,14 @@ console.log(multiplyThree(2,3,4))
 //    or greater than zero, and false otherwise
 function isPositive( number ) {
   if ( number > 0 ){
-    return;
+    return true;
   }
-    return;
+  else {
+    return false;
+  }
 }
+
+
 // Call the function to test each outcome (true & false)
 // Write a separate console.log statement for each outcome
 console.log( 'isPositive - should say true', isPositive(3) );
@@ -50,15 +54,44 @@ console.log( 'isPositive - should say false', isPositive(-3) );
 // 6. Function to return the _last_ item in an array. If the
 //    array is empty, return `undefined`.
 function getLast( array ) {
-
+//  if (array.length === 0) {
+//    return 'undefined';
+//  } else {
+    return array[array.length-1];
+//  }
 }
+
+//I had written the above function to return a string that says 'undefined',
+//however I realized that the function would return 'undefined' without needing
+//an if else statement. However this 'undefined' is technicially not in the form
+//of a string, I think, so I've uncluded my original code in case it should be.
+
+
+let testArray = [1,5,3,6,9,-5,-7,-2];
+let testEmpty = [];
+console.log(getLast(testArray));
+console.log(getLast(testEmpty));
 
 // 7. Function to find a value in an array. Return true if the
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find
-function find( value, array ){
+let i = 0;
 
+function find( value, array ){
+  while (i < array.length){
+    if (array[i] === value){
+      return true;
+    } else {
+      i++;
+    }
+  } return false;
 }
+
+console.log("This should say true: " + find(1, testArray))
+console.log("This should say true: " + find(6, testArray))
+console.log("This should say false: " + find(10, testArray))
+console.log("This should say false: " + find(2, testArray))
+
 
 // ----------------------
 // Stretch Goals
@@ -66,24 +99,62 @@ function find( value, array ){
 // 8. Function to check if a letter is the first letter in a
 //    string. Return true if it is, and false otherwise
 function isFirstLetter(letter, string) {
-
+  return (string.charAt(0) === letter ? true : false);
 }
+
 console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
 console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
 
 // 9. Function to return the sum of all numbers in an array
-function sumAll( ) {
-  let sum = 0
-  // TODO: loop to add items
+
+function sumAll( array ) {
+  let sum = 0;
+  for (let index of array){
+    sum += index;
+  }
   return sum;
 }
+console.log(testArray)
+console.log(sumAll(testArray))
 
 // 10. Function to return a new array of all positive (greater than zero)
 //     numbers contained in an input array. If there are no positive numbers
 //     return an empty array. Note: The input array should not change.
+let sumAllPositive = array => {
+  let sum = 0;
+  for (let index of array){
+    index > 0 ? sum += index : sum += 0;
+  }
+  return (sum > 0 ? sum : [])
+}
+
+let negativeTestArray = [-1, -4, 0, -10]
 
 
+console.log(sumAllPositive(testArray))
+console.log(sumAllPositive(negativeTestArray))
 
 // 11. Pick a problem from Edabit(https://edabit.com/) or
 //     CodeWars(https://www.codewars.com/). Then describe it
 //     here in a comment, write the function, and test it!
+
+//Challenge on edabit states:
+//Create a function that takes an array of numbers and return both the
+//minimum and maximum numbers, in that order.
+
+function minMax(array){
+  let min = array[0]; //Assigning min to first element so function can compare it to something, additionally
+  let max = array[0]; //these two lines allow the function to still work with an array with one value
+  for (index of array){ //for loop to iterate through the array so each element can be compared
+    if (index >= max){ //check if element is greater than or equal to the max value
+      max = index; //if so, assign it to the max value
+    } else if (index <= min){ //check if the element is less than or equal the min value
+      min = index; //if so, assign it to the min value
+    }
+  } return [min, max]; //return the min, then the max value after the array has been looped through
+}
+
+//testing the function with a few different arrays:
+console.log(minMax(testArray))
+console.log(minMax(negativeTestArray))
+console.log(minMax([1]))
